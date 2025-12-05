f6baeaa728cf285124e012051e74fad7e55df70c
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2025-12-05 21:47:44 +0100

    sbatches

--- slurm/submit-sbatch.sh
+++ slurm/submit-sbatch.sh
@@ -20,38 +20,9 @@
 
 # APPROX
 FILES=(
-    "abagraph-st-aspforaba.sh" 
-    "abagraph-st-flexable.sh" 
-    "abagraph-st-flexable-sws.sh" 
-    "abagraph-st-msdis.sh" 
-    "iccma-st-aspforaba.sh" 
-    "iccma-st-flexable.sh" 
-    "iccma-st-flexable-sws.sh" 
-    "iccma-st-msdis.sh" 
-    "abagraph-adm-aspforaba.sh" 
-    "abagraph-adm-flexable.sh" 
-    "abagraph-adm-msdis.sh" 
-    "iccma-adm-aspforaba.sh" 
-    "iccma-adm-flexable.sh" 
-    "iccma-adm-msdis.sh" 
-    # Singleshot
-    "iccma-adm-msdis-singleshot.sh" 
-    "abagraph-adm-msdis-singleshot.sh" 
-    # Approx
-    "iccma-adm-flexable-ap-05.sh"
-    "iccma-adm-flexable-ap-10.sh"
-    "iccma-adm-flexable-ap-25.sh"
-    "iccma-adm-flexable-ap-50.sh"
-    "iccma-adm-flexable-ap-60.sh"
-    "iccma-adm-flexable-ap-70.sh"
-    "iccma-adm-flexable-ap-80.sh"
-    "iccma-adm-flexable-ap-90.sh"
-    "iccma-adm-msdis-ap-05.sh"
-    "iccma-adm-msdis-ap-10.sh"
-    "iccma-adm-msdis-ap-25.sh"
-    "iccma-adm-msdis-ap-50.sh"
-    "iccma-adm-msdis-ap-100.sh"
-    "iccma-adm-msdis-ap-200.sh"
+    "msdis-adm.sh" 
+    "msdis-st.sh" 
+    "nmsl.sh" 
 )
 
 for FILE in "${FILES[@]}"; do
